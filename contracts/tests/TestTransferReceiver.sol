830d1947c16c19042d771e427a9a077a0a159034
David Mihal <david@dmihal.com>
2021-04-12 08:38:37 +0100

    Improve tests

---
+++ contracts/tests/TestTransferReceiver.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2015, 2016, 2017 Dapphub / adapted by [] 2020
+pragma solidity ^0.7.3;
+
+
+contract TestTransferReceiver {
+    event TransferReceived(address token, address sender, uint256 value, bytes data);
+    event ApprovalReceived(address token, address spender, uint256 value, bytes data);
+
+    function onTokenTransfer(address sender, uint value, bytes calldata data) external returns(bool) {
+        emit TransferReceived(msg.sender, sender, value, data);
+        return true;
+    }
+
+    function onTokenApproval(address spender, uint value, bytes calldata data) external returns(bool) {
+        emit ApprovalReceived(msg.sender, spender, value, data);
+        return true;
+    }
+}
--- test/weth11.spec.js
+++ test/weth11.spec.js
@@ -1,11 +1,20 @@
 const { expect } = require("chai");
+const { signERC2612Permit } = require('eth-permit');
+
+const MAX = "115792089237316195423570985008687907853269984665640564039457584007913129639935"
 
 describe("WETH11", function() {
-  let user;
+  let user1;
+  let user2;
+  let user3;
   let weth;
+  let receiver;
 
   before(async () => {
-    ([user] = await ethers.getSigners());
+    ([user1, user2, user3] = await ethers.getSigners());
+
+    const TestTransferReceiver = await ethers.getContractFactory('TestTransferReceiver');
+    receiver = await TestTransferReceiver.deploy();
   });
 
   beforeEach(async () => {
@@ -25,215 +34,229 @@ describe("WETH11", function() {
     })
 
     it('deposits ether', async () => {
-      const balanceBefore = await weth.balanceOf(user1)
-      await weth.deposit({ from: user1, value: 1 })
-      const balanceAfter = await weth.balanceOf(user1)
-      balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+      const balanceBefore = await weth.balanceOf(await user1.getAddress())
+      await weth.deposit({ value: 1 })
+      const balanceAfter = await weth.balanceOf(await user1.getAddress())
+      expect(balanceAfter).to.equal(balanceBefore.add('1'))
     })
 
     it('deposits ether using the legacy method', async () => {
-      const balanceBefore = await weth.balanceOf(user1)
-      await weth.sendTransaction({ from: user1, value: 1 })
-      const balanceAfter = await weth.balanceOf(user1)
-      balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+      const balanceBefore = await weth.balanceOf(await user1.getAddress())
+      await user1.sendTransaction({ to: weth.address, value: 1 })
+      const balanceAfter = await weth.balanceOf(await user1.getAddress())
+      expect(balanceAfter).to.equal(balanceBefore.add('1'))
     })
 
     it('deposits ether to another account', async () => {
-      const balanceBefore = await weth.balanceOf(user2)
-      await weth.depositTo(user2, { from: user1, value: 1 })
-      const balanceAfter = await weth.balanceOf(user2)
-      balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+      const balanceBefore = await weth.balanceOf(await user3.getAddress())
+      await weth.depositTo(user2, { value: 1 })
+      const balanceAfter = await weth.balanceOf(await user3.getAddress())
+      expect(balanceAfter).to.equal(balanceBefore.add('1'))
     })
 
     it('deposits with depositToAndCall', async () => {
-      const receiver = await TestTransferReceiver.new()
-      await weth.depositToAndCall(receiver.address, '0x11', { from: user1, value: 1 })
+      await weth.depositToAndCall(receiver.address, '0x11', { value: 1 })
 
       const events = await receiver.getPastEvents()
       events.length.should.equal(1)
       events[0].event.should.equal('TransferReceived')
       events[0].returnValues.token.should.equal(weth.address)
-      events[0].returnValues.sender.should.equal(user1)
+      events[0].returnValues.sender.should.equal(await user1.getAddress())
       events[0].returnValues.value.should.equal('1')
       events[0].returnValues.data.should.equal('0x11')
     })
 
     describe('with a positive balance', async () => {
       beforeEach(async () => {
-        await weth.deposit({ from: user1, value: 10 })
+        await weth.deposit({ value: 10 })
       })
 
       it('returns the Ether balance as total supply', async () => {
         const totalSupply = await weth.totalSupply()
-        totalSupply.toString().should.equal('10')
+        expect(totalSupply).to.equal('10')
       })
 
       it('withdraws ether', async () => {
-        const balanceBefore = await weth.balanceOf(user1)
-        await weth.withdraw(1, { from: user1 })
-        const balanceAfter = await weth.balanceOf(user1)
-        balanceAfter.toString().should.equal(balanceBefore.sub(new BN('1')).toString())
+        const balanceBefore = await weth.balanceOf(await user1.getAddress())
+        await weth.withdraw(1)
+        const balanceAfter = await weth.balanceOf(await user1.getAddress())
+        expect(balanceAfter).to.equal(balanceBefore.sub('1'))
       })
 
       it('withdraws ether to another account', async () => {
-        const fromBalanceBefore = await weth.balanceOf(user1)
-        const toBalanceBefore = new BN(await web3.eth.getBalance(user2))
+        const fromBalanceBefore = await weth.balanceOf(await user1.getAddress())
+        const toBalanceBefore = ethers.BigNumber.from(await ethers.provider.getBalance(await user2.getAddress()))
 
-        await weth.withdrawTo(user2, 1, { from: user1 })
+        await weth.withdrawTo(await user2.getAddress(), 1)
 
-        const fromBalanceAfter = await weth.balanceOf(user1)
-        const toBalanceAfter = new BN(await web3.eth.getBalance(user2))
+        const fromBalanceAfter = await weth.balanceOf(await user1.getAddress())
+        const toBalanceAfter = ethers.BigNumber.from(await ethers.provider.getBalance(await user2.getAddress()))
 
-        fromBalanceAfter.toString().should.equal(fromBalanceBefore.sub(new BN('1')).toString())
-        toBalanceAfter.toString().should.equal(toBalanceBefore.add(new BN('1')).toString())
+        fromexpect(balanceAfter).to.equal(fromBalanceBefore.sub('1'))
+        toexpect(balanceAfter).to.equal(toBalanceBefore.add('1'))
       })
 
       it('should not withdraw beyond balance', async () => {
-        await expectRevert(weth.withdraw(100, { from: user1 }), 'WETH: burn amount exceeds balance')
-        await expectRevert(weth.withdrawTo(user2, 100, { from: user1 }), 'WETH: burn amount exceeds balance')
-        await expectRevert(weth.withdrawFrom(user1, user2, 100, { from: user1 }), 'WETH: burn amount exceeds balance')
+        await expect(weth.withdraw(100))
+          .to.revertedWith('WETH: burn amount exceeds balance')
+        await expect(weth.withdrawTo(await user2.getAddress(), 100))
+          .to.revertedWith('WETH: burn amount exceeds balance')
+        await expect(weth.withdrawFrom(await user1.getAddress(), await user2.getAddress(), 100))
+          .to.revertedWith('WETH: burn amount exceeds balance')
       })
 
       it('transfers ether', async () => {
-        const balanceBefore = await weth.balanceOf(user2)
-        await weth.transfer(user2, 1, { from: user1 })
-        const balanceAfter = await weth.balanceOf(user2)
-        balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+        const balanceBefore = await weth.balanceOf(await user2.getAddress())
+        await weth.transfer(await user2.getAddress(), 1)
+        const balanceAfter = await weth.balanceOf(await user2.getAddress())
+        expect(balanceAfter).to.equal(balanceBefore.add('1'))
       })
 
       it('withdraws ether by transferring to address(0)', async () => {
-        const balanceBefore = await weth.balanceOf(user1)
-        await weth.transfer('0x0000000000000000000000000000000000000000', 1, { from: user1 })
-        const balanceAfter = await weth.balanceOf(user1)
-        balanceAfter.toString().should.equal(balanceBefore.sub(new BN('1')).toString())
+        const balanceBefore = await weth.balanceOf(await user1.getAddress())
+        const ethBalanceBefore = ethers.BigNumber.from(await ethers.provider.getBalance(await user1.getAddress()))
+
+
+        await weth.transfer('0x0000000000000000000000000000000000000000', 1)
+        const balanceAfter = await weth.balanceOf(await user1.getAddress())
+        const ethBalanceAfter = ethers.BigNumber.from(await ethers.provider.getBalance(await user1.getAddress()))
+
+        expect(balanceAfter).to.equal(balanceBefore.sub('1'))
+        expect(ethBalanceAfter).to.equal(ethBalanceBefore.add('1'))
       })
 
       it('transfers ether using transferFrom', async () => {
-        const balanceBefore = await weth.balanceOf(user2)
-        await weth.transferFrom(user1, user2, 1, { from: user1 })
-        const balanceAfter = await weth.balanceOf(user2)
-        balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+        const balanceBefore = await weth.balanceOf(await user2.getAddress())
+        await weth.transferFrom(await user1.getAddress(), await user2.getAddress(), 1)
+        const balanceAfter = await weth.balanceOf(await user2.getAddress())
+        expect(balanceAfter).to.equal(balanceBefore.add('1'))
       })
 
       it('withdraws ether by transferring from someone to address(0)', async () => {
-        const balanceBefore = await weth.balanceOf(user1)
-        await weth.transferFrom(user1, '0x0000000000000000000000000000000000000000', 1, { from: user1 })
-        const balanceAfter = await weth.balanceOf(user1)
-        balanceAfter.toString().should.equal(balanceBefore.sub(new BN('1')).toString())
+        const balanceBefore = await weth.balanceOf(await user1.getAddress())
+        const ethBalanceBefore = ethers.BigNumber.from(await ethers.provider.getBalance(user1))
+        
+        await weth.transferFrom(await user1.getAddress(), '0x0000000000000000000000000000000000000000', 1)
+        
+        const balanceAfter = await weth.balanceOf(await user1.getAddress())
+        const ethBalanceAfter = ethers.BigNumber.from(await ethers.provider.getBalance(user1))
+        
+        expect(balanceAfter).to.equal(balanceBefore.sub('1'))
+        expect(ethBalanceAfter).to.equal(ethBalanceBefore.add('1'))
       })
 
       it('transfers with transferAndCall', async () => {
-        const receiver = await TestTransferReceiver.new()
-        await weth.transferAndCall(receiver.address, 1, '0x11', { from: user1 })
+        await weth.transferAndCall(receiver.address, 1, '0x11')
 
         const events = await receiver.getPastEvents()
         events.length.should.equal(1)
         events[0].event.should.equal('TransferReceived')
         events[0].returnValues.token.should.equal(weth.address)
-        events[0].returnValues.sender.should.equal(user1)
+        events[0].returnValues.sender.should.equal(await user1.getAddress())
         events[0].returnValues.value.should.equal('1')
         events[0].returnValues.data.should.equal('0x11')
       })
 
       it('should not transfer beyond balance', async () => {
-        await expectRevert(weth.transfer(user2, 100, { from: user1 }), 'WETH: transfer amount exceeds balance')
-        await expectRevert(weth.transferFrom(user1, user2, 100, { from: user1 }), 'WETH: transfer amount exceeds balance')
-        const receiver = await TestTransferReceiver.new()
-        await expectRevert(weth.transferAndCall(receiver.address, 100, '0x11', { from: user1 }), 'WETH: transfer amount exceeds balance')
+        await expect(weth.transfer(await user2.getAddress(), 100))
+          .to.revertedWith('WETH: transfer amount exceeds balance')
+        await expect(weth.transferFrom(await user1.getAddress(), await user2.getAddress(), 100))
+          .to.revertedWith('WETH: transfer amount exceeds balance')
+        await expect(weth.transferAndCall(receiver.address, 100, '0x11'))
+          .to.revertedWith('WETH: transfer amount exceeds balance')
       })
 
       it('approves to increase allowance', async () => {
-        const allowanceBefore = await weth.allowance(user1, user2)
-        await weth.approve(user2, 1, { from: user1 })
-        const allowanceAfter = await weth.allowance(user1, user2)
-        allowanceAfter.toString().should.equal(allowanceBefore.add(new BN('1')).toString())
+        const allowanceBefore = await weth.allowance(await user1.getAddress(), await user2.getAddress())
+        await weth.approve(await user2.getAddress(), 1)
+        const allowanceAfter = await weth.allowance(await user1.getAddress(), await user2.getAddress())
+        allowanceAfter.toString().should.equal(allowanceBefore.add('1'))
       })
 
       it('approves with approveAndCall', async () => {
-        const receiver = await TestTransferReceiver.new()
         await weth.approveAndCall(receiver.address, 1, '0x11', { from: user1 })
 
         const events = await receiver.getPastEvents()
         events.length.should.equal(1)
         events[0].event.should.equal('ApprovalReceived')
         events[0].returnValues.token.should.equal(weth.address)
-        events[0].returnValues.spender.should.equal(user1)
+        events[0].returnValues.spender.should.equal(await user1.getAddress())
         events[0].returnValues.value.should.equal('1')
         events[0].returnValues.data.should.equal('0x11')
       })
 
       it('approves to increase allowance with permit', async () => {
-        const permitResult = await signERC2612Permit(web3.currentProvider, weth.address, user1, user2, '1')
-        await weth.permit(user1, user2, '1', permitResult.deadline, permitResult.v, permitResult.r, permitResult.s)
-        const allowanceAfter = await weth.allowance(user1, user2)
+        const permitResult = await signERC2612Permit(network.provider, weth.address, await user1.getAddress(), await user2.getAddress(), '1')
+        await weth.permit(await user1.getAddress(), await user2.getAddress(), '1', permitResult.deadline, permitResult.v, permitResult.r, permitResult.s)
+        const allowanceAfter = await weth.allowance(await user1.getAddress(), await user2.getAddress())
         allowanceAfter.toString().should.equal('1')
       })
 
       it('does not approve with expired permit', async () => {
-        const permitResult = await signERC2612Permit(web3.currentProvider, weth.address, user1, user2, '1')
-        await expectRevert(weth.permit(
-          user1, user2, '1', 0, permitResult.v, permitResult.r, permitResult.s),
-          'WETH: Expired permit'
-        )
+        const permitResult = await signERC2612Permit(network.provider, weth.address, await user1.getAddress(), await user2.getAddress(), '1')
+        await expect(weth.permit(
+          await user1.getAddress(), await user2.getAddress(), '1', 0, permitResult.v, permitResult.r, permitResult.s),
+          ).to.revertWith('WETH: Expired permit')
       })
 
       it('does not approve with invalid permit', async () => {
-        const permitResult = await signERC2612Permit(web3.currentProvider, weth.address, user1, user2, '1')
-        await expectRevert(
-          weth.permit(user1, user2, '2', permitResult.deadline, permitResult.v, permitResult.r, permitResult.s),
-          'WETH: invalid permit'
-        )
+        const permitResult = await signERC2612Permit(network.provider, weth.address, await user1.getAddress(), await user2.getAddress(), '1')
+        await expect(
+          weth.permit(await user1.getAddress(), await user2.getAddress(), '2', permitResult.deadline, permitResult.v, permitResult.r, permitResult.s),
+          ).to.revertWith('WETH: invalid permit')
       })
 
       describe('with a positive allowance', async () => {
         beforeEach(async () => {
-          await weth.approve(user2, 1, { from: user1 })
+          await weth.approve(await user2.getAddress(), 1)
         })
 
         it('transfers ether using transferFrom and allowance', async () => {
-          const balanceBefore = await weth.balanceOf(user2)
-          await weth.transferFrom(user1, user2, 1, { from: user2 })
-          const balanceAfter = await weth.balanceOf(user2)
-          balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+          const balanceBefore = await weth.balanceOf(await user3.getAddress())
+          await weth.connect(user2).transferFrom(await user1.getAddress(), await user2.getAddress(), 1)
+          const balanceAfter = await weth.balanceOf(await user3.getAddress())
+          expect(balanceAfter).to.equal(balanceBefore.add('1'))
         })
 
         it('should not transfer beyond allowance', async () => {
-          await expectRevert(weth.transferFrom(user1, user2, 2, { from: user2 }), 'WETH: request exceeds allowance')
+          await expect(weth.connect(user2).transferFrom(await user1.getAddress(), await user2.getAddress(), 2))
+            .to.be.revertedWith('WETH: request exceeds allowance')
         })
   
         it('withdraws ether using withdrawFrom and allowance', async () => {
-          const fromBalanceBefore = await weth.balanceOf(user1)
-          const toBalanceBefore = new BN(await web3.eth.getBalance(user3))
+          const fromBalanceBefore = await weth.balanceOf(await user1.getAddress())
+          const toBalanceBefore = ethers.BigNumber.from(await ethers.provider.getBalance(await user3.getAddress()))
 
-          await weth.withdrawFrom(user1, user3, 1, { from: user2 })
+          await weth.connect(user2).withdrawFrom(await user1.getAddress(), await user3.getAddress(), 1)
 
-          const fromBalanceAfter = await weth.balanceOf(user1)
-          const toBalanceAfter = new BN(await web3.eth.getBalance(user3))
+          const fromBalanceAfter = await weth.balanceOf(await user1.getAddress())
+          const toBalanceAfter = ethers.BigNumber.from(await ethers.provider.getBalance(await user3.getAddress()))
 
-          fromBalanceAfter.toString().should.equal(fromBalanceBefore.sub(new BN('1')).toString())
-          toBalanceAfter.toString().should.equal(toBalanceBefore.add(new BN('1')).toString())
+          fromexpect(balanceAfter).to.equal(fromBalanceBefore.sub('1'))
+          toexpect(balanceAfter).to.equal(toBalanceBefore.add('1'))
         })
 
         it('should not withdraw beyond allowance', async () => {
-          await expectRevert(weth.withdrawFrom(user1, user3, 2, { from: user2 }), 'WETH: request exceeds allowance')
+          await expect(weth.connect(user2).withdrawFrom(await user1.getAddress(), await user3.getAddress(), 2))
+            .to.be.revertedWith('WETH: request exceeds allowance')
         })
       })
 
       describe('with a maximum allowance', async () => {
         beforeEach(async () => {
-          await weth.approve(user2, MAX, { from: user1 })
+          await weth.approve(await user2.getAddress(), MAX)
         })
 
         it('does not decrease allowance using transferFrom', async () => {
-          await weth.transferFrom(user1, user2, 1, { from: user2 })
-          const allowanceAfter = await weth.allowance(user1, user2)
-          allowanceAfter.toString().should.equal(MAX)
+          await weth.connect(user2).transferFrom(await user1.getAddress(), await user2.getAddress(), 1)
+          const allowanceAfter = await weth.allowance(await user1.getAddress(), await user2.getAddress())
+          expect(allowanceAfter).to.equal(MAX)
         })
 
         it('does not decrease allowance using withdrawFrom', async () => {
-          await weth.withdrawFrom(user1, user2, 1, { from: user2 })
-          const allowanceAfter = await weth.allowance(user1, user2)
-          allowanceAfter.toString().should.equal(MAX)
+          await weth.connect(user2).withdrawFrom(await user1.getAddress(), await user2.getAddress(), 1)
+          const allowanceAfter = await weth.allowance(await user1.getAddress(), await user2.getAddress())
+          expect(allowanceAfter).to.equal(MAX)
         })
       })
     })
